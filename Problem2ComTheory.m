d1baac39783d08a26ac95efa8925d481b562ca88
Even Flørenæs <even.florenes@gmail.com>
2015-02-24 13:55:25 +0100

    Adding some comments to the code

--- Problem2ComTheory.m
+++ Problem2ComTheory.m
@@ -6,40 +6,39 @@ rho = 0.9;
 
 for k = 1:length(bitrate)
     %Problem 2a
-    sigma_q = sqrt(2^(-2*bitrate(k))*pi*exp(1)/6);
-    f = @(x) (1-rho^2)./((1-rho*exp(-1j*2*pi*x)).*(1-rho*exp(1j*2*pi*x))); %S_X
+    sigma_q = sqrt(2^(-2*bitrate(k))*pi*exp(1)/6); %Noise variance derived in hand notes
+    f = @(x) (1-rho^2)./((1-rho*exp(-1j*2*pi*x)).*(1-rho*exp(1j*2*pi*x))); %S_X defined as function of frequency,x
     sqF = @(x) sqrt(f(x));
-    lagrange = (sigma_q/(sigma_q^2+1))*integral(sqF,-0.5,0.5); %Roten av lambda
+    lagrange = (sigma_q/(sigma_q^2+1))*integral(sqF,-0.5,0.5); %squareroot of lambda
 
     freq = -0.5:0.01:0.5;
-    G = sqrt(sigma_q^2./(lagrange^2.*f(freq)))-(sigma_q^2)./f(freq);
+    G = sqrt(sigma_q^2./(lagrange^2.*f(freq)))-(sigma_q^2)./f(freq); %Making G(f) and H(f) from formulas provided in the compendium
     H = sqrt(lagrange^2*f(freq)/sigma_q^2)-lagrange^2;
 
     fprintf('Squareroot of lagrange: %f, sigma_q: %f, %f bit-rate\n', lagrange, sigma_q, bitrate(k));
-    Sqy = sigma_q^2 * H;
-    Sxy = f(freq).*G.*H;
+    Sqy = sigma_q^2 * H;    %Power spectral density of the noise in the Wiener filter
+    Sxy = f(freq).*G.*H;     %Power spectral density of the signal in the Wiener filter
+
+    
     
     figure(1);
     textG =sprintf('|G(f)|^2 for bit-rate %g', bitrate(k));
     textH =sprintf('|H(f)|^2 for bit-rate %g', bitrate(k));
-    subplot(3,3,3*k-2), plot(freq,G), title(textG);
-        %figure(2);
-    subplot(3,3,3*k-1), plot(freq,H), title(textH);
+    subplot(3,3,3*k-2), plot(freq,G), title(textG); %Plotting the |G(f)|^2 filter
+    subplot(3,3,3*k-1), plot(freq,H), title(textH); %Plotting the |H(f)|^2 filter
 
-    %Sqy = sigma_q*(0.998.*f(freq).^(0.5)-0.00139);
-    %Sxy = 
-    %figure(2+k);
+    
     text = sprintf('Power spectral density of signal(blue) and noise(red) for bit-rate: %g', bitrate(k));
     subplot(3,3,3*k);
-    semilogy(freq, Sqy, 'r'), title(text);
+    semilogy(freq, Sqy, 'r'), title(text); %Plotting the power spectral density of the noise in log-plot
     hold on 
-    semilogy(freq,Sxy, 'b');
+    semilogy(freq,Sxy, 'b'); %Plotting the power spectral density of the signal in log-plot (In the same plot as the corresponding noise
 
-    SNR = 10*log10( sum(Sxy)/sum(Sqy) );
+    SNR = 10*log10( sum(Sxy)/sum(Sqy) ); %Computing signal-to-nois ratio
     fprintf('SNR: %3.4g, %g bit-rate.\n', SNR, bitrate(k));
     %Problem 2b
     Sx = f(freq);
-    FSMG = FrSamp([G(51:end) G(1:50)]);
+    FSMG = FrSamp([G(51:end) G(1:50)]); 
     FSMH = FrSamp([H(51:end) H(1:50)]);
     
     FSMG_fft = fft(FSMG,length(FSMG));
